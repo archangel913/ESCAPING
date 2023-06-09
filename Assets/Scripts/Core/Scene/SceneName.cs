d7306065276d49e0811955af548d5cd258f1b2d1
CreaIce <crew_834@yahoo.co.jp>
2023-06-09 15:34:47 +0900

    [change] シーン名をクラスで管理するように変更

---
+++ Assets/Scripts/Core/Scene/SceneName.cs
@@ -0,0 +1,17 @@
+﻿namespace Escaping.Core
+{
+    /// <summary>
+    /// シーン名一覧クラス
+    /// </summary>
+    public static class SceneName
+    {
+#pragma warning disable SA1401
+#pragma warning disable SA1600
+
+        public static string Title = "TitleScene";
+        public static string Game = "GameScene";
+
+#pragma warning restore SA1600
+#pragma warning restore SA1401
+    }
+}
